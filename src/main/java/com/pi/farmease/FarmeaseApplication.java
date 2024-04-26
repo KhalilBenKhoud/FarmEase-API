156c0b215473c603392aa64629ea52eea32e9a55
Hedi49 <hedi.kramti@esprit.tn>
2024-04-26 09:30:11 +0100

    Statistique added + scheduling

--- src/main/java/com/pi/farmease/FarmeaseApplication.java
+++ src/main/java/com/pi/farmease/FarmeaseApplication.java
@@ -2,8 +2,11 @@ package com.pi.farmease;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
@@ -14,4 +17,9 @@ public class FarmeaseApplication {
         SpringApplication.run(FarmeaseApplication.class, args);
     }
 
+    // Configurer RestTemplate en tant que bean
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
